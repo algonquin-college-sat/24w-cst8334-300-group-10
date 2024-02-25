a4e27180ce3c5bd20a425055943defdbd1c49d29
Bing <xie00076@algonquinlive.com>
2024-02-25 16:30:03 -0500

    Updated some pages

---
+++ script.js
@@ -0,0 +1,10 @@
+// This would be in your main JavaScript file
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('nav.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('nav-placeholder').innerHTML = data);
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('footer-placeholder').innerHTML = data);
+});
